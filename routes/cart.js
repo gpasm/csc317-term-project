71a94775af949cdd9e86c7ab0047cdea15ed3891
gpasm <gpasami@sfsu.edu>
2025-05-21 23:45:45 -0700

    Updated basic cart functions

--- routes/cart.js
+++ routes/cart.js
@@ -2,8 +2,30 @@ const express = require('express');
 const router = express.Router();
 const { requireLogin } = require('./auth');
 
+router.post('/add', (req, res) => {
+    const { id, name, price, image_url } = req.body;
+
+    if(!req.session.cart) {
+        req.session.cart = [];
+    }
+
+    //Checking if item is already in cart?
+    const existingItem = req.session.cart.find(item => item.id === id);
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        req.session.cart.push({ id, name, price: parseFloat(price), image_url, quantity: 1 });
+    }
+
+    res.redirect(req.get('Referer') || '/product'); //so it can stay on products page
+
+})
+
 router.get('/', requireLogin, (req, res) => {
-    res.render('cart');
+    const cart = req.session.cart || [];
+    const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    
+    res.render('cart', {cart, total });
 });
 
 module.exports = router;
