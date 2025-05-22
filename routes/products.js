d71858bfa4f2a313af85df24c5eecb7904f26bd6
gpasm <gpasami@sfsu.edu>
2025-05-21 21:59:05 -0700

    added search function

--- routes/products.js
+++ routes/products.js
@@ -2,31 +2,45 @@ const express = require('express');
 const router = express.Router();
 const db = require('../data/db');
 
-
-
 router.get('/', (req, res) => {
-    db.all("SELECT * FROM products", (err, rows) => {
+    const search = req.query.search || '';                    //for search bar
+    let query = "SELECT * FROM products";
+    let params = [];
+
+    if (search) {
+        query += " WHERE name LIKE ? OR description LIKE ?";
+        const keyword = `%${search.toLowerCase()}%`;
+        params.push(keyword, keyword);
+    }
+
+    console.log("Running query:", query, params);
+
+    // db.all("SELECT * FROM products", (err, rows) => { //original code, edited below
+    db.all(query, params, (err, rows) => {
         if (err) {
             console.log("Error fetching products:", err.message);
             return res.send("Error fetching products");
         }
-        
-        res.render('product', { products: rows });
+
+        res.render('product', { products: rows, search });  //added search
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const productid = req.params.id;
+    db.get("SELECT * FROM products WHERE id=?", [productid], (err, rows) => {
+        if (err) {
+            console.log("Error fetching products:", err.message);
+            return res.send("There's been an Error");
+        }
+        res.render('product-detail', {
+            product: rows,
+            search: ''
+        });
     });
 });
 
 
 
-
-router.get('/:id',(req,res)=>{
-    const productid=req.params.id;
-    db.get("SELECT * FROM products WHERE id=?",[productid], (err, rows) => {
-        if (err){
-            console.log("Error fetching Products",err.message);
-            return res.send("Theres been an Error");
-        }
-        res.render('product-detail',{product:rows});
-        });
-});
 module.exports = router;
 
